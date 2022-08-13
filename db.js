39c693209360a90783768f81485fc9ec3dc913bb
marcleyc <marcleycc@gmail.com>
2022-08-13 16:44:37 +0100

    first commit
    first commit

---
+++ db.js
@@ -0,0 +1,152 @@
+
+  var aptos = [
+        {
+          item: 1,
+          freguesia: 'Lousã Vilarinho',
+          valor: "129.000€",
+          quartos: 2,
+          banheiro: 2,
+          m2: 134.92,
+          garagem: 'sim',
+          eficiencia: 'C',
+          imobiliaria: 'Lindão Premium 923.299.100',
+          foto:'https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6IjFnZ3NoeGhpOGduNjEtRUNPU1lTVEVNIiwidyI6W3siZm4iOiI0amFmcGw5aDNmZ3QtQVBUIiwicyI6IjE0IiwicCI6IjEwLC0xMCIsImEiOiIwIn1dfQ.p813eGE6PjEB2MAQDN7Nb5WogQrnPgBc55a62at6y4s/image;s=1280x1024;q=80',
+          link:'https://www.imovirtual.com/pt/anuncio/apartamento-t3-no-centro-da-lousa-ID19igb.html#b2a181266b',
+          descricao:'No reconhecido Condomínio Alcântara Rio, projectado pelo Arquitecto Frederico Valsassina, com Menção Honrosa nos Prémios Valmor e galardoado como melhor empreendimento na categoria Habitação, na 8ª edição dos Óscares do Imobiliário, encontramos este maravilhoso T2, com vista Rio. No empreendimento encontra agradáveis jardins privativos que lhe conferem um requinte e comodidade numa zona privilegiada da cidade.Este apartamento dispõe de um amplo hall de entrada de 9m2 que nos leva à sala, de 27m2, muito luminosa e com vista. A cozinha, totalmente equipada, com 14,50 m2 conta com caldeira a gás, forno eléctrico, placa, máquina de lavar loiça e roupa e tem ainda uma zona de despensa.Já na zona de noite, encontramos dois quartos de 14m2 e 16m2, um deles em suite.O apartamento tem ar condicionado e tem incluído um lugar de garagem.Uma casa perto do Rio, rodeada de espaços verdes, comércio e serviços, tudo perto de si.'
+        },
+        {
+          item: 2,
+          freguesia: 'Lousã Vilarinho',
+          valor: "238.000€",
+          quartos: 4,
+          banheiro: 3,
+          m2: 320,
+          garagem: 'sim',
+          eficiencia: 'A',
+          imobiliaria: 'Remax 239 702 323',
+          foto:'https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6ImF3MHpkajVtMXlkbzItRUNPU1lTVEVNIiwidyI6W3siZm4iOiI0amFmcGw5aDNmZ3QtQVBUIiwicyI6IjE0IiwicCI6IjEwLC0xMCIsImEiOiIwIn1dfQ.heXcdfs6Rss2P5R_5vk4dSTcxYryrWuNWtODm9qOR6U/image;s=1280x1024;q=80',
+          link:'https://www.imovirtual.com/pt/anuncio/moradia-t4-para-venda-ID18qb3.html#d12bddcf43',
+          descricao:'No reconhecido Condomínio Alcântara Rio, projectado pelo Arquitecto Frederico Valsassina, com Menção Honrosa nos Prémios Valmor e galardoado como melhor empreendimento na categoria Habitação, na 8ª edição dos Óscares do Imobiliário, encontramos este maravilhoso T2, com vista Rio. No empreendimento encontra agradáveis jardins privativos que lhe conferem um requinte e comodidade numa zona privilegiada da cidade.Este apartamento dispõe de um amplo hall de entrada de 9m2 que nos leva à sala, de 27m2, muito luminosa e com vista. A cozinha, totalmente equipada, com 14,50 m2 conta com caldeira a gás, forno eléctrico, placa, máquina de lavar loiça e roupa e tem ainda uma zona de despensa.Já na zona de noite, encontramos dois quartos de 14m2 e 16m2, um deles em suite.O apartamento tem ar condicionado e tem incluído um lugar de garagem.Uma casa perto do Rio, rodeada de espaços verdes, comércio e serviços, tudo perto de si.'
+        },
+        {
+          item: 3,
+          freguesia: 'Lousã Vilarinho',
+          valor: "249.000",
+          quartos: 3,
+          banheiro: 3,
+          m2: 139,
+          garagem: 'sim',
+          eficiencia: 'A+',
+          imobiliaria: 'Remax',
+          foto:'https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6IjcxeWl0NDFrcGUxcy1FQ09TWVNURU0iLCJ3IjpbeyJmbiI6IjRqYWZwbDloM2ZndC1BUFQiLCJzIjoiMTQiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.0hXIhGWzKzOFrwUfYHnUDRjp7sAZXTFc9O-i8ngch2U/image;s=1280x1024;q=80',
+          link:'https://www.imovirtual.com/pt/anuncio/moradia-t4-para-venda-ID18qaU.html#d12bddcf43',
+          descricao:'No reconhecido Condomínio Alcântara Rio, projectado pelo Arquitecto Frederico Valsassina, com Menção Honrosa nos Prémios Valmor e galardoado como melhor empreendimento na categoria Habitação, na 8ª edição dos Óscares do Imobiliário, encontramos este maravilhoso T2, com vista Rio. No empreendimento encontra agradáveis jardins privativos que lhe conferem um requinte e comodidade numa zona privilegiada da cidade.Este apartamento dispõe de um amplo hall de entrada de 9m2 que nos leva à sala, de 27m2, muito luminosa e com vista. A cozinha, totalmente equipada, com 14,50 m2 conta com caldeira a gás, forno eléctrico, placa, máquina de lavar loiça e roupa e tem ainda uma zona de despensa.Já na zona de noite, encontramos dois quartos de 14m2 e 16m2, um deles em suite.O apartamento tem ar condicionado e tem incluído um lugar de garagem.Uma casa perto do Rio, rodeada de espaços verdes, comércio e serviços, tudo perto de si.'
+        },
+        {
+          item: 4,
+          freguesia: 'Foz de Arouce',
+          valor: "263.000",
+          quartos: 4,
+          banheiro: 3,
+          m2: 232,
+          garagem: 'sim',
+          eficiencia: 'A+',
+          imobiliaria: 'Remax',
+          foto:'https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6ImdtMDZxNzdva2k5OC1FQ09TWVNURU0iLCJ3IjpbeyJmbiI6IjRqYWZwbDloM2ZndC1BUFQiLCJzIjoiMTQiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.qJu73-ICjb6BBxUGotohd-Lsm3m_i1ppGv0yBaivZ-Q/image;s=1280x1024;q=80',
+          link:'https://www.imovirtual.com/pt/anuncio/moradia-t4-para-venda-ID18qaU.html#d12bddcf43',
+          descricao:'No reconhecido Condomínio Alcântara Rio, projectado pelo Arquitecto Frederico Valsassina, com Menção Honrosa nos Prémios Valmor e galardoado como melhor empreendimento na categoria Habitação, na 8ª edição dos Óscares do Imobiliário, encontramos este maravilhoso T2, com vista Rio. No empreendimento encontra agradáveis jardins privativos que lhe conferem um requinte e comodidade numa zona privilegiada da cidade.Este apartamento dispõe de um amplo hall de entrada de 9m2 que nos leva à sala, de 27m2, muito luminosa e com vista. A cozinha, totalmente equipada, com 14,50 m2 conta com caldeira a gás, forno eléctrico, placa, máquina de lavar loiça e roupa e tem ainda uma zona de despensa.Já na zona de noite, encontramos dois quartos de 14m2 e 16m2, um deles em suite.O apartamento tem ar condicionado e tem incluído um lugar de garagem.Uma casa perto do Rio, rodeada de espaços verdes, comércio e serviços, tudo perto de si.'
+        }, 
+        {
+          item: 5,
+          freguesia: 'Lousã Vilarinho',
+          valor: "245.000",
+          quartos: 5,
+          banheiro: 3,
+          m2: 340,
+          garagem: 'sim',
+          eficiencia: 'trâmite',
+          imobiliaria: 'RuiCaravalho 914.881.432 ref.31454935',
+          foto:'./img/5.jpg',
+          link:'https://www.idealista.pt/imovel/31454935/',
+          descricao:'No reconhecido Condomínio Alcântara Rio, projectado pelo Arquitecto Frederico Valsassina, com Menção Honrosa nos Prémios Valmor e galardoado como melhor empreendimento na categoria Habitação, na 8ª edição dos Óscares do Imobiliário, encontramos este maravilhoso T2, com vista Rio. No empreendimento encontra agradáveis jardins privativos que lhe conferem um requinte e comodidade numa zona privilegiada da cidade.Este apartamento dispõe de um amplo hall de entrada de 9m2 que nos leva à sala, de 27m2, muito luminosa e com vista. A cozinha, totalmente equipada, com 14,50 m2 conta com caldeira a gás, forno eléctrico, placa, máquina de lavar loiça e roupa e tem ainda uma zona de despensa.Já na zona de noite, encontramos dois quartos de 14m2 e 16m2, um deles em suite.O apartamento tem ar condicionado e tem incluído um lugar de garagem.Uma casa perto do Rio, rodeada de espaços verdes, comércio e serviços, tudo perto de si.'
+        },
+        {
+          item: 6,
+          freguesia: 'Lousã Vilarinho',
+          valor: "244.000",
+          quartos: 5,
+          banheiro: 3,
+          m2: 232,
+          garagem: 'sim',
+          eficiencia: 'C',
+          imobiliaria: 'Osvaldo 961.656.136 ref.31937145',
+          foto:'./img/6.jpg',
+          link:'https://www.idealista.pt/imovel/31937145/',
+          descricao:'',
+        },
+        {
+          item: 7,
+          freguesia: 'Miranda do Corvo',
+          valor: "196.000",
+          quartos: 4,
+          banheiro: 3,
+          m2: 261,
+          garagem: 'sim',
+          eficiencia: 'A',
+          imobiliaria: 'IAD 223.289.184 ref.72533',
+          foto:'./img/7.jpg',
+          link:'https://www.idealista.pt/imovel/31893828/',
+          descricao:'',
+        },   
+        {
+          item: 0,
+          freguesia: '',
+          valor: "000.000",
+          quartos: 0,
+          banheiro: 3,
+          m2: 0,
+          garagem: 'sim',
+          eficiencia: 'V',
+          imobiliaria: 'Osvaldo 961.656.136 ref.31937145',
+          foto:'https://xrei.com/images/mapa-regioes-de-portugal-scaled.jpg',
+          link:'',
+          descricao:'',
+        },            
+        
+  ];
+
+
+  // -------------------------------------------------------E V E N T O S
+      var array2 = aptos.map(function(item, indice){
+        return `<tr>
+                    <td>${item.item}</td>
+                    <td>${item.freguesia}</td>
+                    <td>${item.valor}</td>
+                    <td><a href=${item.link}>click</a></td>
+                </tr>`;
+       });
+
+
+      //var array = aptos.map(function(item){
+              
+              //for(var i=0; i < array.length; i++){
+              //console.log(array[i]);
+              //document.getElementById('foto').src=`${item.foto}`;              
+             // }
+                
+      // });
+
+      document.body.onload = inserir;
+            
+       function inserir(){
+
+        for(var i=0; i < aptos.length; i++){
+              console.log(aptos.item);
+              document.getElementById('foto').src=`${aptos.foto}`;              
+              }
+          //document.getElementById('foto').src='https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6InJob241ZXRjYnU5eS1FQ09TWVNURU0iLCJ3IjpbeyJmbiI6IjRqYWZwbDloM2ZndC1BUFQiLCJzIjoiMTQiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.-vCSWJW7OJSIjhlOQbXLH2KMBzcze6Kzh10mPR_8-lo/image;s=1280x1024;q=80';
+          console.log('rodando');          
+       };
+
+       //inserir();
+       
