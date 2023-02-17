8224f48e34ef6df94d1ce2dbca0424239643303f
marcleyc <marcleytech@gmail.com>
2023-02-17 14:32:09 +0000

    algarve pages

---
+++ alessandro.js
@@ -0,0 +1,51 @@
+var aptos = [    
+    {
+    id:1, local:"", valor:510000, tipologia:"T3", ee:"A", situacao:"vendido", 
+    imobiliaria:"Arcada - Larissa tel:289.176.990 ref:1110588B",
+    comissao:"17.5%", agendamento: "20/02/2023 às 11hs",
+    link:"https://www.idealista.pt/imovel/31990578/",
+    obs:"Falei com Paulo o gerente", end:"",
+    },
+    {    
+    id:2, local:"", valor:755000, tipologia:"T3", ee:"B", situacao:"não aceita parceria",
+    imobiliaria: "vap Joana 289.176.584 ref:VAP151332",
+    comissao:"0", agendamento:"",
+    link:"https://www.idealista.pt/imovel/29855990/",
+    obs:"Não aceita parceria", end:"",
+    },
+    { 
+    id:3, local:"", valor:525000, tipologia:"T3", ee:"A", situacao:"vendido",
+    imobiliaria:"house team 265.145.971 ref:120612091-86",
+    comissao:"", agendamento:"",
+    link:"https://www.idealista.pt/imovel/31679910/",
+    obs:"", end:"",   
+    },
+    {
+    id:4, local:"", valor:510000, tipologia:"T3", ee:"A", situacao:"agendado",
+    imobiliaria:"top imoveis - Alexandrina Martins 289.176.289 ref:APA_879",
+    comissao:"30 40 ou 50%", agendamento:"20/02/2023 às 15hs",
+    link:"https://www.idealista.pt/imovel/32227787/",
+    obs:"", end:"",
+    },
+    {
+    id:5, local:"", valor:695000, tipologia:"T3", ee:"B", situacao:"agendado",
+    imobiliaria:"alamo Vânia 289.176.203 ref:AMO_206",
+    comissao:"50%", agendamento:"20/02/2023 às 13hs",
+    link:"https://www.idealista.pt/imovel/32057590/",
+    obs:"", end:"",
+    },
+    {
+    id:6, local:"", valor:510000, tipologia:"T3", ee:"A", situacao:"vendido",
+    imobiliaria:"predimed - Ana Alba 917.664.509 ref:CasaSAPO_2021146BGQ",
+    comissao:"", agendamento:"",
+    link:"https://casa.sapo.pt/comprar-apartamento-t3-loule-quarteira-cee65474-2246-11ec-a521-060000000056.html",
+    obs:"", end:"",
+    },
+    {
+    id:7, local:"Albufeira e Olhos de Água", valor:525000, tipologia:"T3", ee:"A+", situacao:"aguardando retorno",
+    imobiliaria:"Immo Algarve - Cecília ref:A-317 961.243.308",
+    comissao:"", agendamento:"",
+    link:"https://www.idealista.pt/imovel/32453009/",
+    obs:"", end:"",
+    },
+]
