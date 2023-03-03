98f4be12696436f44a330bee9126b02544cce9da
marcleyc <marcleytech@gmail.com>
2023-03-03 17:41:00 +0000

    planta

--- alessandro.js
+++ alessandro.js
@@ -4,77 +4,77 @@ var aptos = [
         imobiliaria:"HomeTown ",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/31645293/",
-        obs:"Só o R/C disponível", end:"", cor:"primary",
+        obs:"Só o R/C disponível", end:"", cor:"primary", planta:"warning",
     },
     {
         id:2, local:"Portimão / Oasis Mar Lt. 1", valor:"625.000€", tipologia:"T3", ee:"", garagem:2, situacao:"aguardando retorno", 
         imobiliaria:"Paula 910.141.320",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/32130149/",
-        obs:"A anunciante não atende", end:"", cor:"warning",
+        obs:"A anunciante não atende", end:"", cor:"warning", planta:"warning",
     },
     {
         id:3, local:"Portimão / 3 castelos", valor:"670.000€", tipologia:"T3", ee:"B", garagem:"2", situacao:"Disponível / Pronto", 
         imobiliaria:"Predimed Francisco 289.176.565 - Miguel 961.738.941",
         comissao:"50%", agendamento: "", 
         link:"https://www.idealista.pt/imovel/31891922/",
-        obs:"Somente esse apto disponível, aguardando envio da planta", end:"", cor:"primary",
+        obs:"Somente esse apto disponível, aguardando envio da planta", end:"", cor:"primary", planta:"primary",
     },
     {
         id:4, local:"Portimão / rua Estrela do mar", valor:"790.000€", tipologia:"T3", ee:"A", garagem:"", situacao:"Disponível / Pronto", 
         imobiliaria:"",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/31045109/",
-        obs:"Não trabalha com imobiliárias", end:"", cor:"danger",
+        obs:"Não trabalha com imobiliárias", end:"", cor:"danger", planta:"warning",
     },
     {
         id:5, local:"Portimão / Alto do Quintão", valor:"390.000€", tipologia:"T3", ee:"A", garagem:"", situacao:"VENDIDO", 
         imobiliaria:"Nuris NU-5170",
         comissao:"", agendamento: "",
-        link:"https://www.idealista.pt/imovel/32051846/",
-        obs:"", end:"", cor:"danger",
+        link:"https://www.idealista.pt/imovel/32051846/", 
+        obs:"", end:"", cor:"danger", planta:"warning"
     },
     {
         id:6, local:"Portimão / Alto do Quintão, Av do Brasil 24", valor:"410.000€", tipologia:"T3", ee:"A", garagem:"", situacao:"VENDIDO", 
         imobiliaria:"KW Flash 289.176.437 Paulo Jorge ref.1210-2531",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/32482449/",
-        obs:"", end:"", cor:"danger",
+        obs:"", end:"", cor:"danger", planta:"warning"
     },
     {
         id:7, local:"Portimão / rua Maria Eugénia Silva Horta", valor:"498.000€", tipologia:"T4", ee:"C", garagem:"", situacao:"VENDIDO", 
         imobiliaria:"Vista Plaza - Vera Santos 282.095.816",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/32254088/",
-        obs:"", end:"", cor:"danger",
+        obs:"", end:"", cor:"danger", planta:"warning"
     },
     {
         id:8, local:"Portimão / Amparo - Alto do Quintão", valor:"365.000€", tipologia:"T3", ee:"B", garagem:"", situacao:"Disponível / Pronto", 
-        imobiliaria:"Mimosa - 282.095.848 ref.VA584",
+        imobiliaria:"Mimosa - 282.095.848 ref.VA584 Vitor 966.981.803",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/31288387/",
-        obs:"Aguardando detalhes do apto", end:"", cor:"warning",
+        obs:"Aguardando detalhes do apto", end:"", cor:"warning", planta:"primary"
     },
     {
         id:9, local:"Portimão / beco Padre Arsénio Castro da Silva", valor:"429.000€", tipologia:"T3", ee:"A+", garagem:"1", situacao:"Disponível / Entrega em 3 meses", 
-        imobiliaria:"AlgarveHome 289.176.809 ref.APA_173 João Duarte  ",
+        imobiliaria:"AlgarveHome 289.176.809 ref.APA_173 João Duarte 937.921.543",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/32337789/",
-        obs:"Só tem um imóvel no 9º andar. É fácil estacionar o 2º carro do lado de fora do prédio", end:"", cor:"primary",
+        obs:"Só tem um imóvel no 9º andar. É fácil estacionar o 2º carro do lado de fora do prédio", end:"", cor:"primary", planta:"warning"
     },
     {
         id:10, local:"Portimão / Amparo - Alto do Quintão", valor:"470.000€", tipologia:"T4", ee:"não tem", garagem:"", situacao:"VENDIDO", 
         imobiliaria:"Springer Properties 289.176.864 ref.SP_1894-1",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/31882589/",
-        obs:"", end:"", cor:"danger",
+        obs:"", end:"", cor:"danger", planta:"warning"
     },
     {
         id:"mapa", local:"", valor:"", tipologia:"", ee:"", garagem:"", situacao:"", 
         imobiliaria:"",
         comissao:"", agendamento: "",
         link:"",
-        obs:"", end:"", cor:"light",
+        obs:"", end:"", cor:"light", planta:"warning"
     },
     
 ]
