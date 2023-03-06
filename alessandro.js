087549445ce718ec2fd4367050d4f78b4ca15468
marcleyc <marcleytech@gmail.com>
2023-03-06 16:17:49 +0000

    arvores

--- alessandro.js
+++ alessandro.js
@@ -53,7 +53,7 @@ var aptos = [
         imobiliaria:"Mimosa - 282.095.848 ref.VA584 Vitor 966.981.803",
         comissao:"", agendamento: "",
         link:"https://www.idealista.pt/imovel/31288387/",
-        obs:"Na praça central tem um projeto de paqrque infantil", end:"", cor:"warning", planta:"primary"
+        obs:"Na praça central tem um projeto de parque infantil, hoje possui somente árvores", end:"", cor:"warning", planta:"primary"
     },
     {
         id:9, local:"Portimão / Terraços da Cidade - beco Padre Arsénio Castro da Silva", valor:"429.000€", tipologia:"T3", ee:"A+", garagem:"1", situacao:"Disponível / Entrega em 3 meses", 
