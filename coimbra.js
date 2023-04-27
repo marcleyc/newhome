98b3ef48adda2baf66a5493c1494973900f5bdf6
marcleyc <marcleytech@gmail.com>
2023-04-27 17:00:45 +0100

    Wilmar

---
+++ coimbra.js
@@ -0,0 +1,32 @@
+var aptos = [    
+    {
+        id:1, local:"Santo António dos Olivais", valor:"220.000€", tipologia:"T3", ee:"E", garagem:1, situacao:"", 
+        imobiliaria:"comprarCasa ",
+        comissao:"", agendamento: "",
+        link:"https://www.idealista.pt/imovel/32414865/",
+        obs:"550m da escola da Clarinha", end:"", cor:"primary", planta:"warning",
+    },
+    {
+        id:2, local:"Celas", valor:"220.000€", tipologia:"T2", ee:"D", garagem:"não", situacao:"", 
+        imobiliaria:"",
+        comissao:"", agendamento: "",
+        link:"https://www.idealista.pt/imovel/32607439/foto/15/",
+        obs:"750m da escola da Clarinha", end:"", cor:"light", planta:"warning"
+    },
+    {
+        id:3, local:"", valor:"250.000€", tipologia:"T4", ee:"D", garagem:"", situacao:"", 
+        imobiliaria:"",
+        comissao:"", agendamento: "",
+        link:"https://www.idealista.pt/imovel/32493725/",
+        obs:"", end:"", cor:"light", planta:"warning"
+    },
+    {
+        id:"mapa", local:"", valor:"", tipologia:"T4", ee:"", garagem:"", situacao:"", 
+        imobiliaria:"",
+        comissao:"", agendamento: "",
+        link:"",
+        obs:"", end:"", cor:"light", planta:"warning"
+    },
+    
+]
+
