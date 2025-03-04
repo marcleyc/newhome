9bd635db0f426b19551e98d0dd3d6545264f6ad3
marcleyc <marcleycc@gmail.com>
2025-03-04 23:12:54 +0000

    Update db.js

--- coimbra/db.js
+++ coimbra/db.js
@@ -1,35 +1,35 @@
 var aptos = [    
     {
-        id:1, local:"Baixa", valor:"118.000€", tipologia:"T0", ee:"", garagem:"", 
+        id:1, local:"Forum", valor:"245.000€", tipologia:"T1", ee:"A", garagem:"", 
         situacao:"Disponível", 
-        imobiliaria:"HomeLusa",
-        comissao:"", agendamento: "",
-        link:"https://www.idealista.pt/imovel/33251058/",
+        imobiliaria:"Civilria",
+        comissao:"4%", agendamento: "05/03/2025 às 11hs",
+        link:"https://www.civilria.pt/imovel/heritage/22132877",
+        obs:"Flávio Carvalho - 961.327.792", end:"", cor:"primary", planta:"primary",
+    },
+    {
+        id:2, local:"Forum", valor:"360.000€", tipologia:"T2", ee:"A", garagem:"", 
+        situacao:"Disponível", 
+        imobiliaria:"Civilria",
+        comissao:"4%", agendamento: "05/03/2025 às 11hs",
+        link:"https://www.civilria.pt/imovel/heritage/22132830",
         obs:"", end:"", cor:"primary", planta:"primary",
     },
     {
-        id:2, local:"rua Particular, Ladeira do Batista", valor:"125.000€", tipologia:"T0", ee:"B", garagem:"", 
-        situacao:"Disponível", 
-        imobiliaria:"RuiSilva RemaxVisão",
-        comissao:"", agendamento: "",
-        link:"https://www.idealista.pt/imovel/33326330/",
-        obs:"", end:"", cor:"primary", planta:"primary",
-    },
-    {
-        id:3, local:"zona de Montes Claros e a Praça da República", valor:"75.000€", tipologia:"T0", ee:"D", garagem:"1", 
+        id:3, local:"Baixa - Sé Nova", valor:"282.000€", tipologia:"T2", ee:"B", garagem:"?", 
         situacao:"Disponível / Pronto", 
-        imobiliaria:"KW Frederik Buys",
-        comissao:"", agendamento: "",
-        link:"https://www.idealista.pt/imovel/32948189/",
+        imobiliaria:"Remax - Cláudia",
+        comissao:"", agendamento: "05/03/2025 ás 15hs",
+        link:"https://casa.sapo.pt/comprar-apartamento-t2-coimbra-se-nova-santa-cruz-almedina-e-sao-bartolomeu-c958a9c5-f0e3-11ef-8add-060000000054.html",
         obs:"", end:"", cor:"primary", planta:"primary",
     },
     {
-        id:4, local:"Coimbra", valor:"000.000€", tipologia:"T", ee:"", garagem:"", 
-        situacao:"VENDIDO", 
-        imobiliaria:"",
-        comissao:"", agendamento: "",
-        link:"", 
-        obs:"", end:"", cor:"danger", planta:"warning"
+        id:4, local:"Terraços do Rio", valor:"350.000€", tipologia:"T2", ee:"", garagem:"", 
+        situacao:"Disponível", 
+        imobiliaria:"Era Imobiliária",
+        comissao:"30% de 4% Sidonio Barros", agendamento: "05/03/2025 ás 16hs",
+        link:"https://www.era.pt/imovel/apartamento-t1-terraco-coimbra-terracos-do-rio-115250101", 
+        obs:"", end:"", cor:"primary", planta:"warning"
     },
     
 ]
